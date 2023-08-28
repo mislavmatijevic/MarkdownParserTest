c1190b94d245a298a1e5bf531a2a233504e9b098
Mislav Matijević <mmatijevi@foi.hr>
2023-08-28 14:12:05 +0200

    Replace special HTML characters.

--- Converters/Markdown2HtmlConverter.cs
+++ Converters/Markdown2HtmlConverter.cs
@@ -4,7 +4,18 @@
     {
         public string Convert(string value)
         {
+            value = ReplaceHtmlSpecificChars(value);
             return value;
+        }
+
+        private string ReplaceHtmlSpecificChars(string value)
+        {
+            return value
+                .Replace("\"", "&#34;")
+                .Replace("\'", "&#39;")
+                .Replace("&", "&#38;")
+                .Replace("<", "&#60;")
+                .Replace(">", "&#62;");
         }
     }
 }
