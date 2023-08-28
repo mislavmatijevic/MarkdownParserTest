716a3b6fa2cefd60bffe1fcf62c64e5d78526e3c
Mislav Matijević <mmatijevi@foi.hr>
2023-08-28 10:39:55 +0200

    Add TextsViewModel for keeping track on inputted text.

---
+++ ViewModels/TextsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MarkdownParserTest.ViewModels
+{
+    internal class TextsViewModel
+    {
+        public string MarkdownText { get; set; } = "";
+        public string HTMLText { get; set; } = "";
+    }
+}
